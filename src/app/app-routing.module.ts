4486ad0204f8e2a25282b93e495fd1b16b1c9d1b
UsamaNawaz1 <usamanawaz1994@gmail.com>
2022-06-04 18:53:02 +0500

    adding new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersCreateComponent } from './components/users-create/users-create.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { UsersEditComponent } from './components/users-edit/users-edit.component';
+
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AuthGuard } from "./service/auth.guard";
+
 const routes: Routes = [
+  
   { path: '', pathMatch: 'full', redirectTo: 'create-user' },
   { path: 'create-user', component: UsersCreateComponent },
   { path: 'edit-user/:id', component: UsersEditComponent },
-  { path: 'users-list', component: UsersListComponent }  
+  { path: 'users-list', component: UsersListComponent },
+  {path: 'log-in', component: SigninComponent },
+  { path: 'sign-up', component: SignupComponent },  
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,22 @@ import { UsersEditComponent } from './components/users-edit/users-edit.component
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ApiService } from './service/api.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
+
+import { AuthInterceptor } from './service/authconfig.interceptor';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     UsersCreateComponent,
     UsersEditComponent,
     UsersListComponent,
+    SigninComponent,
+    SignupComponent,
+    UserProfileComponent,
     
   ],
   imports: [
@@ -23,8 +32,15 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    HttpClientModule
   ],
-  providers: [ApiService],
+  providers: [ApiService, 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
